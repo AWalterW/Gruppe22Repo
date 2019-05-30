79a04f59bfd9fd1aa2e5700ad83e95a4381388d7
Andreas Walter Wilhelmsen <wilhelmsen1@live.no>
2019-05-30 10:53:47 +0200

    Small changes

--- script/script.js
+++ script/script.js
@@ -103,12 +103,5 @@ function addTask(title, description, checkList) {
     tasks.push(newTask); 
     sortTasks();
 }  
-
-function dragStart(ev) {
-    ev.dataTransfer.effectAllowed = 'move';
-    ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
-    ev.dataTransfer.setDragImage(ev.target,0,0);
-    
-    return true;
-}
+`
 
