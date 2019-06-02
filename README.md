8395d9fddb1315a66617311d876bc4e8b4d6e8d1
Andreas Walter Wilhelmsen <wilhelmsen1@live.no>
2019-06-02 16:20:16 +0200

    fixing merge errors

--- README.md
+++
@@ -1,9 +0,0 @@
-# KanBarn
-A KanBan webapp by Gruppe 22
-
-
-![Kanban board]()
-
-## Demo:
-https://stupefied-mirzakhani-c2f2f8.netlify.com
-
--- script/members.js
+++
@@ -1,9 +0,0 @@
-let members = [
-    {
-        id: 0, 
-        email: "noen@eksempel.no", 
-        password: "abc123", 
-        name: "Ola Nordmann"
-        
-    }
-];
